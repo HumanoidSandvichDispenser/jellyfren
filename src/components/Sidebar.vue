1da94c71e076f3d812d086bef68e605930fc15fd
HumanoidSandvichDispenser <humanoidsandvichdispenser@gmail.com>
2022-12-25 19:57:24 -0800

    sidebar icons

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,62 +1,116 @@
 <script setup lang="ts">
-import { computed } from "vue";
+import { computed, ref } from "vue";
 import { RouterLink } from "vue-router";
 import store from "../store";
+import router from "../router";
 
 const libraries = computed(() => store.state.libraries);
+const isOpen = ref(false);
 </script>
 
 <template>
-    <aside class="sidebar">
+    <aside :class="{ sidebar: true, open: isOpen }">
+        <button @click="isOpen = !isOpen">
+            <span class="icon">
+                <bootstrap-icon icon="list" />
+            </span>
+            <span class="text">Menu</span>
+        </button>
         <router-link to="/home">
-            <button>Home</button>
+            <button>
+                <span class="icon">
+                    <bootstrap-icon icon="house-fill" />
+                </span>
+                <span class="text">Home</span>
+            </button>
         </router-link>
         <router-link to="/nowplaying">
-            <button>Now Playing</button>
+            <button>
+                <span class="icon">
+                    <bootstrap-icon icon="headphones" />
+                </span>
+                <span class="text">Now Playing</span>
+            </button>
         </router-link>
-        <h1>Libraries</h1>
-        <router-link
-            v-for="library in libraries"
-            :to="'/library/' + library.Id"
-        >
-            <button>{{ library.Name }}</button>
-        </router-link>
+        <h1 v-if="isOpen">Libraries</h1>
+        <div v-if="isOpen">
+            <router-link
+                v-for="library in libraries"
+                :to="'/library/' + library.Id"
+            >
+                <button>
+                    <span class="icon">
+                        <bootstrap-icon icon="collection-play-fill" />
+                    </span>
+                    <span class="text">{{ library.Name }}</span>
+                </button>
+            </router-link>
+        </div>
     </aside>
 </template>
 
 <style>
-.content > aside.sidebar {
-    flex-basis: 192px;
+aside.sidebar {
+    flex-basis: 8px;
     flex-grow: 0;
     flex-shrink: 0;
     background-color: var(--bg-dark);
 }
 
-.content > aside.sidebar.open {
-    flex-basis: 64px;
+aside.sidebar.open {
+    flex-basis: 256px;
+}
+
+aside.sidebar .text {
+    display: none;
+    vertical-align: middle;
+}
+
+aside.sidebar.open .text {
+    margin-left: 24px;
+    display: inline;
+}
+
+aside.sidebar a:hover {
+    text-decoration: none;
 }
 
 aside.sidebar button {
     display: block;
+    max-height: 48px;
     width: 100%;
     text-align: left;
     border: unset;
-    padding: 16px 32px;
     font-weight: 500;
+    padding: 12px 24px;
+    border-radius: 0;
+    box-shadow: none;
+    margin: 0;
 }
 
 aside.sidebar button:hover {
+    background-color: var(--bg1);
     border: unset;
 }
 
-aside.sidebar button.a {
-    display: block;
-    width: 100%;
+aside.sidebar button .icon {
+    display: inline-block;
+    vertical-align: middle;
+}
+
+aside.sidebar button .icon > svg {
+    display: inline-block;
+    vertical-align: middle;
+}
+
+aside.sidebar .router-link-active button {
+    background-color: var(--bg0);
 }
 
 aside.sidebar h1 {
     font-size: 14px;
     text-transform: uppercase;
     padding: 8px;
+    padding-left: 16px;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import "./style.css";
 import App from "./App.vue";
 import store from "./store";
 import router from "./router";
+import BootstrapIcon from "@dvuckovic/vue3-bootstrap-icons";
 
 store.dispatch.jellyfin.ensureInit();
 
 createApp(App)
     .use(store.original)
     .use(router)
+    .use(BootstrapIcon)
     .mount("#app");
