d470cddc43259a8ce6023d87110aec804dc47b55
HumanoidSandvichDispenser <humanoidsandvichdispenser@gmail.com>
2022-12-31 19:33:54 -0800

    removed old store files

--- src/store/modules/settings/index.ts
+++
@@ -1,8 +0,0 @@
-import { createStore, StoreOptions } from "vuex";
-import SettingsState from "./settings-state";
-
-export default createStore({
-    state: {
-
-    }
-} as StoreOptions<SettingsState>);
